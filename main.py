b4dd4f9d95fc80be2eeba669df73efa123c02f5a
-info <vladyslavkrupin240707@gmail.com>
2023-11-05 21:30:30 +0000

    2.3
    sep & end

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
-Hello one
-testing
+text = "big data"
+print(text[3:])
+all_numbers = '0123456789'
+print(all_numbers[:: 2])
+
+print('s', 't', 'u', sep='&', end='\n')
+
+one = input()
+two = input()
+tree = input()
+four = input()
+print(two,tree,four, sep = one, end = '\n')
